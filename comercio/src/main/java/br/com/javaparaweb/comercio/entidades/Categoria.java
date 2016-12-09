982c227c1c079e748851421b3376feb820ec3cca
Anderson <a.valadares@gmail.com>
2016-12-08 23:08:26 -0200

    incluisão do projeto exemplo - comercio

---
+++ comercio/src/main/java/br/com/javaparaweb/comercio/entidades/Categoria.java
@@ -0,0 +1,9 @@
+package br.com.javaparaweb.comercio.entidades;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 4761805689019386635L;
+
+}
